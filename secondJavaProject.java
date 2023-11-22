fd7dfe2f83d2bc07087de054928e7507ba2e366d
Frank <Trivem@users.noreply.github.com>
2023-11-22 14:50:04 -0600

    Add files via upload

---
+++ secondJavaProject.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+//imported code for it to tell the date and time
+public class Main {
+    public static void main(String[] args) {
+
+        Date now = new Date ();
+        //calling upon the uploaded code for date and time
+
+                System.out.println(now);
+                //should be printing out in the terminal what the date and time is accurately.
+
+    }
+}
